6adfb9cff68dc3657a8ee732576e9dbaeedcf450
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-03-02 17:56:44 +0800

    存储箱重量的计算设计

---
+++ learnning/knowledges/1_chapter/2_volume.c
@@ -0,0 +1,21 @@
+//
+// Created by NfryCreator on 2023/3/2.
+//
+#include <stdio.h>
+
+int main(void) {
+    float volume, length, width, height;
+    int weight;
+    length = 12.0f;
+    width = 10.0f;
+    height = 8.0f;
+    volume = length * width * height;
+    weight = volume/166;
+    weight++;
+    printf("Dimensions: %.1fx%.1fx%.1f\n",length,width,height);
+    printf("Volume (cubes inches): %.1f\n",volume);
+    printf("the calculate result is : %f\n",volume/166);
+    printf("Dimensional weight (pounds): %d\n",weight);
+
+    return 1;
+}
