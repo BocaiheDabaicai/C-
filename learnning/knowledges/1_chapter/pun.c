77236bcd6a8669b0aad3f86face1e4e80d768c87
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-03-02 17:00:56 +0800

    初始化

---
+++ learnning/knowledges/1_chapter/pun.c
@@ -0,0 +1,11 @@
+//
+// Created by NfryCreator on 2023/3/2.
+//
+#include <stdio.h>
+
+int main(void ){
+    float a = 0.1f;
+    printf("this number is : %f",a);
+//    printf("To be or not to be, \nthis is a question.\n");
+    return 1;
+}
