2cc8dfe721753109430cabb5e5a183da9d62b9d6
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-03-03 09:36:53 +0800

    计算容积（初始化值、手动输入）

--- learnning/knowledges/1_chapter/3_volume_optimization.c
+++ learnning/knowledges/1_chapter/3_volume_optimization.c
@@ -6,13 +6,16 @@
 int main(void) {
     float volume, length, width, height;
     int weight;
-    length = 12.0f;
-    width = 10.0f;
-    height = 8.0f;
+    printf("Enter length of box: ");
+    scanf("%f",&length);
+    printf("Enter width of box: ");
+    scanf("%f",&width);
+    printf("Enter height of box: ");
+    scanf("%f",&height);
     volume = length * width * height;
     weight = volume/166;
     weight++;
-    printf("Dimensions: %.1fx%.1fx%.1f\n",length,width,height);
+    printf("Dimensions: %.1f x %.1f x %.1f\n",length,width,height);
     printf("Volume (cubes inches): %.1f\n",volume);
     printf("the calculate result is : %.2f\n",volume/166);
     printf("Dimensional weight (pounds): %d\n",weight);
